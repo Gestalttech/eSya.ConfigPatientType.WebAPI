c4588aeb97f964d55235bed95110ceac83b74838
AbdulR <abdulr@gestalttechindia.in>
2024-02-03 16:38:33 +0530

    New Project created

---
+++ eSya.ConfigPatientType.IF/eSya.ConfigPatientType.IF/ICommonDataRepository.cs
@@ -0,0 +1,16 @@
+﻿using eSya.ConfigPatientType.DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSya.ConfigPatientType.IF
+{
+    public interface ICommonDataRepository
+    {
+        Task<List<DO_ApplicationCodes>> GetApplicationCodesbyCodeType(int codetype);
+        Task<List<DO_ApplicationCodes>> GetApplicationCodesByCodeTypeList(List<int> l_codeType);
+        Task<List<DO_BusinessLocation>> GetBusinessKey();
+    }
+}
---
+++ eSya.ConfigPatientType.WebAPI/eSya.ConfigPatientType.WebAPI/Controllers/CommonMethodController.cs
@@ -0,0 +1,35 @@
+﻿using eSya.ConfigPatientType.IF;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSya.ConfigPatientType.WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CommonMethodController : ControllerBase
+    {
+        private readonly ICommonDataRepository _commonDataRepository;
+        public CommonMethodController(ICommonDataRepository commonDataRepository)
+        {
+            _commonDataRepository = commonDataRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetApplicationCodesbyCodeType(int codetype)
+        {
+            var appcodes = await _commonDataRepository.GetApplicationCodesbyCodeType(codetype);
+            return Ok(appcodes);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetApplicationCodesByCodeTypeList(List<int> l_codeType)
+        {
+            var ac = await _commonDataRepository.GetApplicationCodesByCodeTypeList(l_codeType);
+            return Ok(ac);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBusinessKey()
+        {
+            var appcodes = await _commonDataRepository.GetBusinessKey();
+            return Ok(appcodes);
+        }
+    }
+}
